c00406b6af57ebde7ed4d9fcedf5fa8de99f63ce
MINGI-SONG\송민기 <mgsong9194@gmail.com>
2026-04-08 21:17:31 +0900

    49. How to automate Java/JavaScript Alert popups with Playwright

--- tests/MoreValidations.spec.js
+++ tests/MoreValidations.spec.js
@@ -1,8 +1,17 @@
 const {test, expect} = require('@playwright/test');
 
 test('Popup validations', async({page}) => {
+    // handle hidden
     await page.goto("https://rahulshettyacademy.com/AutomationPractice/");
     await expect(page.locator("#displayed-text")).toBeVisible();
     await page.locator("#hide-textbox").click();
     await expect(page.locator("#displayed-text")).toBeHidden();
-})
+
+    // handle popup
+    page.on('dialog', dialog => dialog.accept()); // event listener
+    await page.locator("#confirmbtn").click();
+    
+    // handle hover
+    await page.pause();
+    await page.locator("#mousehover").hover();
+});
