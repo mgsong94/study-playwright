32900b724d7acc6a35ca5586c542722c53113cdf
mingi <mgsong9194@gmail.com>
2026-04-20 09:36:25 +0900

    90. various options in use property & setting up Project configurations

---
+++ playwright.config1.mjs
@@ -0,0 +1,44 @@
+// @ts-check
+import { defineConfig, devices } from '@playwright/test';
+
+/**
+ * Read environment variables from file.
+ * https://github.com/motdotla/dotenv
+ */
+// import dotenv from 'dotenv';
+// import path from 'path';
+// dotenv.config({ path: path.resolve(__dirname, '.env') });
+
+/**
+ * @see https://playwright.dev/docs/test-configuration
+ */
+export default defineConfig({
+  testDir: './tests',
+  timeout: 30 * 1000,
+  expect: {
+    timeout: 5000
+  },
+  projects: [
+    {
+      name: 'chrome',
+      use: {
+        browserName: 'chromium',
+        headless: false,
+        screenshot: 'on',
+        trace: 'retain-on-failure' // off, on, retry-on-first? // trace 를 통해서 api request, response 확인 가능.
+      },
+    }, 
+    {
+      name: 'safari',
+      use: {
+        browserName: 'webkit',
+        headless: true,
+        screenshot: 'on',
+        trace: 'retain-on-failure' // off, on, retry-on-first? // trace 를 통해서 api request, response 확인 가능.
+      },
+    }
+  ],
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  reporter: 'html',
+});
+
